817a5bed7fc424355dceb9369c871cb0f788492a
lnfnet <lnfnet2005@163.com>
2020-05-19 08:43:03 +0800

    Create sqlist - 副本.c

---
+++ dataStruct/sqlist - 副本.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+#define datatype int
+#define error printf
+
+#define sqlist_maxsize 40
+
+typedef struct 
+{
+	datatype data[sqlist_maxsize];
+	int last;
+	
+}SqList;
+
+void insert_sqlist(SqList L,datatype x ,int i)
+{
+	if(L.last==sqlist_maxsize)
+		error("表满");
+	if((i<1)||(i>L.last+1))
+		error("非法位置");
+	for(int j=L.last;j=i;j--)
+	{
+		L.data[j] = L.data[j-1];
+	  	L.data[i-1]=x;
+		L.last=L.last+1; 
+	}
+		
+}
+
+void delete_sqlist(SqList L,int i)
+{
+	if((i<1)||(i>L.last)) 
+		error("非法位置");
+	for(int j =i+1;j=L.last;j++)
+	{	
+		L.data[j-2]=L.data[j-1];
+		L.last=L.last -1;
+				}		
+}
+
+void print_sqlist(SqList L)
+{
+	for (int i=0;i<L.last-1;i++)
+		printf("%d",L.data[i]);
+}
+
+int local_sqlist()
+{
+	
+}
+void main()
+{
+	SqList L; 
+	L.last=0;
+	 //initiate L
+	insert_sqlist(L,10,1);
+	insert_sqlist(L,20,2);
+	
+	print_sqlist(L);
+	
+}
