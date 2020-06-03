6c70843f3f30b2e121145467fd6ec7175021ce74
lnfnet <lnfnet2005@163.com>
2020-06-03 20:27:16 +0800

    123

---
+++ testC/tutorlas.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <float.h>
+
+//#define LENGTH 10 
+#define WIDTH1  5
+#define NEWLINE '\n'
+
+
+// Variable declaration:
+extern int a, b;
+extern int c;
+extern float f;
+
+// define
+
+
+int main(int argc, char** argv) {
+//data type 
+    printf("CHAR_BIT    :   %d\n", CHAR_BIT);
+    printf("CHAR_MAX    :   %d\n", CHAR_MAX);
+    printf("CHAR_MIN    :   %d\n", CHAR_MIN);
+    printf("INT_MAX     :   %d\n", INT_MAX);
+    printf("INT_MIN     :   %d\n", INT_MIN);
+    printf("LONG_MAX    :   %ld\n", (long) LONG_MAX);
+    printf("LONG_MIN    :   %ld\n", (long) LONG_MIN);
+    printf("SCHAR_MAX   :   %d\n", SCHAR_MAX);
+    printf("SCHAR_MIN   :   %d\n", SCHAR_MIN);
+    printf("SHRT_MAX    :   %d\n", SHRT_MAX);
+    printf("SHRT_MIN    :   %d\n", SHRT_MIN);
+    printf("UCHAR_MAX   :   %d\n", UCHAR_MAX);
+    printf("UINT_MAX    :   %u\n", (unsigned int) UINT_MAX);
+    printf("ULONG_MAX   :   %lu\n", (unsigned long) ULONG_MAX);
+    printf("USHRT_MAX   :   %d\n", (unsigned short) USHRT_MAX);
+
+  /* variable definition: */
+   int a, b;
+   int c;
+   float f;
+ 
+   /* actual initialization */
+   a = 10;
+   b = 20;
+  
+   c = a + b;
+   printf("value of c : %d \n", c);
+
+   f = 70.0/3.0;
+   printf("value of f : %f \n", f);
+   
+   //const 
+   const int  LENGTH = 10;
+   const int  WIDTH = 5;
+   const char NEWLINE = '\n';
+   int area;  
+   
+   area = LENGTH * WIDTH;
+   printf("value of area : %d", area);
+   printf("%c", NEWLINE);
+
+    return 0;
+}
