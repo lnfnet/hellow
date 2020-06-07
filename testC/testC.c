23f9fc006e2e61ba326254156c1c4cd14096142d
lnfnet <lnfnet2005@163.com>
2020-06-07 23:15:24 +0800

    1

--- testC/testC.c
+++ testC/testC.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#define BUF_LEN 256
+#define INIT_NSTR 2
+#define NSTR_INCR 2
 
 
 #define ENDLINE '\n'
+
+//函数原型
+char* str_in();
+void str_sort(const char**,size_t);
+void swap(const char**,const char**);
+void str_out(const char* const*,size_t);
+void free_memory(char**,size_t);
 
 
 enum enumtest{test1,test2};
@@ -124,8 +136,84 @@ int  main(void)
 		//cant mallock the memory for this code.
 		printf("can not malloc memory...please check !");
 		}
+	////////////////////指针的综合实例
+	size_t pS_size = INIT_NSTR;
+	char **pS = calloc(pS_size,sizeof(char*));
+	if(!pS)
+	{	
+		printf("failed to allocate memory for string ");
+		exit(1);		
+	}
+	char **pTemb = NULL;
+	size_t str_count = 0;
+	char *pStr = NULL;
+	printf("Enter one strying per line.press Enter to end:\n");
+
+	while((pStr = str_in() != NULL)
+	{
+		if(str_count == pS_size)
+		{
+			pS_size + = NSTR_INCR;
+			if(!(pTemp = realloc(ps,pS_size*sizeof(char*))))
+			{
+				printf("Memory allocation for array of strings failed.\n");
+				return 2;	
+			}
+			pS = pTemp;
+		}
+	str_sort(pS,str_count);
+	str_out(pS,str_count);
+	free_memory(pS,str_count);
+
+	}
 	
+///////////////////////////////////////////////////////////////	
+//[函数]
+	bool SendMessage(const char* pmessage)  //不能修改char值
+	{
+		//code to send the message
+		return true;
+	}
+///////////////////////////////////////////////////
 	
-///////////////////////////////////////////////////////////////		
         return 0;
+}
+
+
+char* str_in(void)
+{
+	char buf[BUF_LEN];
+	if(!gets_s(buf,BUF_LEN)
+	{
+		prinf("\n Error reading string.\n");
+		return NULL;
+	}
+	if(buf[0]=='\0')
+		return NULL;
+	size_t str_len = strnlen_s(buf,BUF_LEN)+1;
+	char *pString = malloc(str_len);
+	
+	if(!pString)
+	{
+		printf("memory allocation failure.\n");
+	}
+	
+}
+
+void str_sort(const char**,size_t)
+{
+}
+
+void swap(const char**,const char**)
+{
+}
+
+void str_out(const char* const*,size_t)
+{
+
+}
+
+void free_memory(char**,size_t)
+{
+
 }
