11221afd3b5d8e0ff3d93029ea3316c0a583b7b9
lnfnet <lnfnet2005@163.com>
2020-12-25 00:39:13 +0800

    123

--- code/cgi-bin/mysqlTest.py
+++ code/cgi-bin/mysqlTest.py
@@ -1,7 +1,8 @@
-#!c:\python27\python
+#!c:\python27\python.exe
 # -*- coding: UTF-8 -*-
-
+import cgi
 import MySQLdb
+
 
 # 打开数据库连接
 db = MySQLdb.connect("localhost", "root", "gkimfmnui", "testDB", charset='utf8' )
@@ -10,7 +11,7 @@ db = MySQLdb.connect("localhost", "root", "gkimfmnui", "testDB", charset='utf8' 
 cursor = db.cursor()
 
 # 如果数据表已经存在使用 execute() 方法删除表。
-cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
+result=cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
 
 # 创建数据表SQL语句
 sql = """CREATE TABLE EMPLOYEE (
@@ -22,5 +23,82 @@ sql = """CREATE TABLE EMPLOYEE (
 
 cursor.execute(sql)
 
+
+
+print "Content-type:text/html"
+print
+print "<html>"
+print "<head>"
+print "<meta charset=\"utf-8\">"
+print "<title>菜鸟教程 CGI 测试实例</title>"
+print "</head>"
+print "<body>"
+if result:
+    print("<h2>drop the EMPLOYEE table</h2>")
+else:
+    print "<h2>EMPLOYEE not exists,create now....</h2>"
+print "<h2> 数据库存已建</h2>" 
+print "</body>"
+print "</html>"
+
+# SQL 插入语句
+sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
+         LAST_NAME, AGE, SEX, INCOME)
+         VALUES ('Mac', 'Mohan', 20, 'M', 2000)"""
+try:
+   # 执行sql语句
+   cursor.execute(sql)
+   # 提交到数据库执行
+   db.commit()
+except:
+   # Rollback in case there is any error
+   db.rollback()
+
+
+sql = "SELECT * FROM EMPLOYEE \
+       WHERE INCOME > %s" % (1000)
+try:
+   # 执行SQL语句
+   cursor.execute(sql)
+   # 获取所有记录列表
+   results = cursor.fetchall()
+   for row in results:
+      fname = row[0]
+      lname = row[1]
+      age = row[2]
+      sex = row[3]
+      income = row[4]
+      # 打印结果
+      print "fname=%s,lname=%s,age=%s,sex=%s,income=%s" % \
+             (fname, lname, age, sex, income )
+except:
+   print "Error: unable to fecth data"
+
+# SQL 更新语句
+sql = "UPDATE EMPLOYEE SET AGE = AGE + 1 WHERE SEX = '%c'" % ('M')
+try:
+   # 执行SQL语句
+   cursor.execute(sql)
+   # 提交到数据库执行
+   db.commit()
+except:
+   # 发生错误时回滚
+   db.rollback()
+
+
+# SQL 删除语句
+sql = "DELETE FROM EMPLOYEE WHERE AGE > %s" % (20)
+try:
+   # 执行SQL语句
+   cursor.execute(sql)
+   # 提交修改
+   db.commit()
+except:
+   # 发生错误时回滚
+   db.rollback()
+
+
 # 关闭数据库连接
 db.close()
+
+
